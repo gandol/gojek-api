afd611d06722751b4ce4fece3ec4a4a9bf90ce7f
err <err@gandol.err>
2021-12-29 10:34:43 +0700

    fix: update exaple

--- example/tes.js
+++ example/tes.js
@@ -1,10 +1,32 @@
-const {GopayApi,GojekApi} = require('../index');
+const { v4 } = require('uuid');
+const { GopayApi, GojekApi } = require('../index');
 
 
 (async () => {
-    let authTOken = "token_here";
+    let authTOken = "token here";
 
-    const gopayApi = new GopayApi(authTOken)
-    const gojekApi = new GojekApi(authTOken)
-    const dataResult = await gopayApi.createPayment("qrisStringHere","pinHere")
+    // const gopayApi = new GopayApi(authTOken)
+    // await gojekApi.setAuthHeader(authTOken)
+    let jalan = true
+    try {
+        const gojekApi = new GojekApi()
+        // await gojekApi.setSessionId(v4())
+        await gojekApi.setdeviceId("30f68e910b5f69r0")
+        let sessionId = v4()
+        await gojekApi.setSessionId(sessionId)
+        // await gojekApi.getOtpRegist("email","the account name","628377777777")
+        // await gojekApi.validateOtpRegist("0000","the token from otp regist")
+
+        const data = await gojekApi.loginRequest("85850332817")//without +62 or 62
+        console.log(data);
+        // const loginRequest = await gojekApi.getRefreshTOken(dataRefresh)
+        // if (loginRequest.access_token) {
+        //     console.log(loginRequest.access_token);
+        //     jalan = false
+
+        // }
+        // console.log(loginRequest);
+    } catch (error) {
+
+    }
 })()
